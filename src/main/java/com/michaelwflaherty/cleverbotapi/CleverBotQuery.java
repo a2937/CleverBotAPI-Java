d3b0dc13103eee2d7649d306257483262c23777f
Michael Flaherty <michaelwflaherty@me.com>
2017-02-11 21:34:10 -0800

    Add Javadocs

--- src/main/java/com/michaelwflaherty/cleverbotapi/CleverBotQuery.java
+++ src/main/java/com/michaelwflaherty/cleverbotapi/CleverBotQuery.java
@@ -21,9 +21,22 @@ import java.net.*;
 import java.io.*;
 import com.google.gson.*;
 
+/**
+ * A CleverBot Query
+ *
+ * A <code>CleverBotQuery</code> object contains all of the necessary tools to
+ * query CleverBot API's servers
+ *
+ * @author Michael W Flaherty
+ * @version 1.0.2
+ */
 public class CleverBotQuery
 {
     /* Constants */
+	
+	/**
+	* URL String used for CleverBot connection
+	*/
     public static final String URL_STRING = "http://www.cleverbot.com/getreply?key=";
 
     /* Instance Variables */
@@ -34,52 +47,103 @@ public class CleverBotQuery
     private int random;
 
     /* Getters & Setters */
+	
+	/**
+	* Gets the input phrase
+	*
+	* @return String object containing the input phrase
+	*/
     public String getPhrase()
     {
         return this.phrase;
     }
 
+	/**
+	* Sets the input phrase.
+	*
+	* @param phrase input phrase to be sent to CleverBot
+	*/
     public void setPhrase(String phrase)
     {
         this.phrase = phrase;
     }
 
+	/**
+	* Gets the response from a request. Must be used after <code>sendRequest()</code>
+	*
+	* @return String object containing the CleverBot response
+	*/
     public String getResponse()
     {
         return this.response;
     }
 
+	/**
+	* Sets the response value. Only used internally
+	*
+	* @param response CleverBot response
+	*/
     private void setResponse(String response)
     {
         this.response = response;
     }
 
+	/**
+	* Gets the API key
+	*
+	* @return String object containing the API key
+	*/
     public String getAPIKey()
     {
         return this.key;
     }
 
+	/**
+	* Sets the unique conversation identifier
+	*
+	* @param conversationID unique conversation identifier
+	*/
     public void setConversationID(String conversationID)
     {
         this.conversationID = conversationID;
     }
 
+	/**
+	* Sets the random number. Only used internally
+	*
+	* @param number number to set
+	*/
     private void setRandomNumber(String number)
     {
         this.random = Integer.parseInt(number);
     }
 
+	/**
+	* Gets the random number
+	*
+	* @return random number as an integer
+	*/
     public int getRandomNumber()
     {
         return this.random;
     }
 
+	/**
+	* Gets conversation identifier
+	*
+	* @return String object containing the conversation identifier
+	*/
     public String getConversationID()
     {
         return this.conversationID;
     }
 
-    public void sendRequest()throws IOException
+	/**
+	* Sends request to CleverBot servers. API key and phrase should be set prior to this call
+	*
+	* @throws IOException exception upon query failure
+	*/
+    public void sendRequest() throws IOException
     {
         /* Create & Format URL */
         URL url = new URL(CleverBotQuery.formatRequest(CleverBotQuery.URL_STRING, this.getAPIKey(), this.getPhrase(), this.getConversationID()));
@@ -101,6 +165,12 @@ public class CleverBotQuery
         in.close(); // close!
     }
 
+	/**
+	* CleverBotQuery constructor
+	*
+	* @param key API key (cleverbot.com/api) 
+	* @param phrase input phrase
+	*/
     public CleverBotQuery(String key, String phrase)
     {
         this.key = key;
@@ -109,6 +179,15 @@ public class CleverBotQuery
     }
 
     /* Helper Methods */
+	/**
+	* URL request formater
+	*
+	* @param url starting url to connect to 
+	* @param key API key (cleverbot.com/api)
+	* @param phrase input to be sent to CleverBot servers
+	* @param conversationID unique conversation identifer
+	* @return String object containing properly formatted URL
+	*/
     private static String formatRequest(String url, String key, String phrase, String conversationID)
     {
         String formattedPhrase = phrase.replace(' ', '+');
